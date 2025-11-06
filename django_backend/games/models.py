e3bfdd045164e6017f1767180948b7a0e36ffc62
Albi Xhafa <55335339+albixhafa@users.noreply.github.com>
2025-11-05 19:23:27 -0800

    Delete django_backend directory

--- django_backend/games/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Game(models.Model):
-    title = models.CharField(max_length=100)
-    location = models.CharField(max_length=255)
-    start_time = models.DateTimeField()
-
-    def __str__(self):
-        return f"{self.title} at {self.location}"
--- django_backend/games/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Game
-
-def game_list(request):
-    games = Game.objects.all()
-    return render(request, 'games/list.html', {'games': games})
